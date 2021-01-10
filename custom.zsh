4a5341887147bf51990aafb7aee0c50f922e7327
Gary Koehl <gkoehl.slalom@gmail.com>
2021-01-10 16:33:04 -0600

    Updating.

--- custom.zsh
+++ custom.zsh
@@ -1,21 +1,27 @@
-# ROOT
-export FS_ROOT_PATH="${HOME}/fs"
-
+################################################################################
 # FS
-# FS: VARIABLES
-export FS_BIN_PATH="${FS_ROOT_PATH}/bin"
-export FS_DOCS_PATH="${FS_ROOT_PATH}/docs"
-export FS_REPOS_PATH="${FS_ROOT_PATH}/repos"
-export FS_SCRIPTS_PATH="${FS_ROOT_PATH}/scripts"
-export FS_SHELL_PATH="${FS_ROOT_PATH}/shells/zsh"
+################################################################################
+export FS_PATH="${HOME}/fs"
+export FS___PATH="${FS_PATH}/_"
+export FS____PATH="${FS_PATH}/__"
+export FS_BIN_PATH="${FS_PATH}/bin"
+export FS_DOCS_PATH="${FS_PATH}/docs"
+export FS_REPOS_PATH="${FS_PATH}/repos"
+export FS_SCRIPTS_PATH="${FS_PATH}/scripts"
+export FS_SHELLS_PATH="${FS_PATH}/shells"
+export FS_SHELL_PATH="${FS_SHELLS_PATH}/zsh"
 export FS_SHELL_THEMES_PATH="${FS_SHELL_PATH}/themes"
 export FS_SHELL_PLUGINS_PATH="${FS_SHELL_PATH}/plugins"
 
+################################################################################
 # PATH
+################################################################################
 export PATH="/usr/local/sbin:${PATH}"
 export PATH="${PATH}:${FS_BIN_PATH}"
 
+################################################################################
 # SERVICES
+################################################################################
 # SERVICES: AWS
 export AWS_PAGER=jq
 # SERVICES: ASDF
@@ -23,16 +29,22 @@ fpath=(${ASDF_DIR}/completions $fpath)
 autoload -Uz compinit
 compinit
 
+################################################################################
 # ALIASES
+################################################################################
 # alias reload="source ~/.zshrc"
 # alias awsacct="aws sts get-caller-identity --query Account --output text"
 # alias awsaccti="aws sts get-caller-identity | cat"
 
+################################################################################
 # EXECUTE
+################################################################################
 # eval $(thefuck --alias)
 # [[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
 
+################################################################################
 # SOURCES
+################################################################################
 function source_if_exists() {
     if [[ ! -f "${1}" ]]; then
         return
